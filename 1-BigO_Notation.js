996a15a8885b33a90700c09e0cedd8891da2a1a4
MSA <crystalbluetech@gmail.com>
2023-12-05 14:17:47 +0100

    O of N examples

--- 1-BigO_Notation.js
+++ 1-BigO_Notation.js
@@ -1,58 +1,94 @@
-// Big O Notation
-// Big O Notation is a tool used to describe the time complexity of algorithms. It calculates the time taken to run an algorithm as the input grows. In other words, it calculates the worst-case time complexity of an algorithm. Big O Notation in Data Structure describes the upper bound of an algorithm's runtime.
+// Big O is : So big-O is just a way of formalized fuzzy counting,
 
-/*An logrithm is the inverse of exponensiation
+// It allows us to talk in a very formal way about how the runtime of an algorithm grows as the inputs grows. It's a way of describing the relationship between the input to a function or as it grows and how that changes the runtime of that function.
+//   The relationship between the input size
+// and then the time relative to that input.
 
-Whats is Log
 
-log2  8 = 3
 
-log2 (value) = exponent =========>  2 exponent = value
-  */
+// We say that an algorithm is O(f(n)) if the number of simple operations the computer has to do is eventually less than a constant times f(n), as n increases
+  
+       
+// *    f(n) could be linear (f(n) = n) 
+// * f(n) could be quadratic (f(n) = n2)
+// *   f(n) could be constant (f(n) = 1)
+// *   f(n) could be something entirely different!
 
-/*Recursion has to do with space complexity not time */
+// ￼
 
-function addUpTo(n){
+// ￼
+
+// ￼
+// // this prints in pairs
+// function printAllPairs (n) {
+//   for (let i = 0; i < n; i++) {
+//     for (let j  = 0; j < n; j++){
+//       console.log(i, j);
+//     }
+//   }
+// }
+
+// printAllPairs(5)
+
+
+// EXAMPLE 1  
+function addUpTo(n) {
+  return n * (n + 1) / 2;
+}
+// Always 3 operations
+
+// O(1)
+
+
+
+// EXAMPLE 2
+function addUpTo(n) {
   let total = 0;
-  for(let i = 0; i <= n; i++){
+  for (let i = 1; i <= n; i++) {
     total += i;
   }
   return total;
 }
 
+// Number of operations is (eventually) bounded by a multiple of n (say, 10n)
 
-let t1 = performance.now();
-addUpTo(1000000000);
-let t2 = performance.now();
-console.log(`Time Elapsed: ${(t2 -t1) / 1000 } seconds`);
+// O(n)
 
 
-function addUpFast(fn){
-return fn * (fn + 1) / 2;
+// EXAMPLE 3 
+
+function countUpAndDown(n) {
+  console.log("Going up!");
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+  }
+  console.log("At the top!\nGoing down...");
+  for (let j = n - 1; j >= 0; j--) {
+    console.log(j);
+  }
+  console.log("Back down. Bye!");
 }
 
-  // Explained
-  // 6 * (6 + 1)/2
+countUpAndDown(2)
 
-let t3 = performance.now();
-addUpFast(1000000000);
-let t4 = performance.now();
-console.log(`Time Elapsed: ${(t3 -t4) / 1000 } seconds`);
-  
-  // The addUpFast function takes a single parameter num, which is assumed to be a positive integer. The function then calculates the sum of all the numbers from 1 to num, inclusive, using a formula based on the arithmetic series.
-  
-  // The formula used in the function is:
-  
-  // num * (num + 1) / 2
-  
-  // This formula calculates the sum of an arithmetic series from 1 to num. It works by taking advantage of the fact that the sum of the numbers from 1 to n is equal to n times the average of the first and last numbers. In this case, the first number is 1 and the last number is num, so the average is (1 + num) / 2. Multiplying this by num gives the sum of the arithmetic series.
-  
-  // For example, if num is 5, the function calculates the sum of the numbers from 1 to 5 as:
-  
-  // 5 * (5 + 1) / 2 = 5 * 6 / 2 = 15
-  
-  // This is the sum of the numbers 1, 2, 3, 4, and 5.
-  
-  // The addUpFast function is a simple and efficient way to calculate the sum of a large number of consecutive integers, as it does not require iterating through each number in the sequence. Instead, it uses a mathematical formula to directly calculate the sum in constant time./*
-  
-   // If you want to calculate the sum of a different range, just change the number in the parentheses.
+// Number of operations is (eventually) bounded by a multiple of n (say, 10n)
+
+// O(n)Number of operations is (eventually) bounded by a multiple of n (say, 10n)
+
+// O(n)
+
+
+// EXAMPLE 4 (here we have 2 o(n) thats N2)
+function printAllPairs(n) {
+  for (var i = 0; i < n; i++) {
+    for (var j = 0; j < n; j++) {
+      console.log(i, j);
+    }
+  }
+}
+
+// O(n) operation inside of an O(n) operation.
+
+// O(n * n)
+
+// O(n 2 )
