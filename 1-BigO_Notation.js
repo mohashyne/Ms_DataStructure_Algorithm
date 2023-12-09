71ae183f4c5bad4c3d6313f2cfb2c266dcfa6e17
MSA <crystalbluetech@gmail.com>
2023-12-09 23:50:27 +0100

    BigO of N Objects

--- 1-BigO_Notation.js
+++ 1-BigO_Notation.js
@@ -1,71 +1,26 @@
+/* 
+Big 0 of Objects
+-Insertion - O(1)
+-Removal - O(1)
+-Searching - O(N)
+-Access - 0(1)
+When you don't need any ordering, objects are an excellent choice!
+*/
 
-// function countCharacters(str) {
-//   const counts = {};
-//   for (let i = 0; i < str.length; i++) {
-//     // const char = str.charAt(i);
-//     const char = str[i].toLowerCase();
-//        if (char === " ") {
-//       continue; // skip white space character
-//     }
-//     else if(counts[char] > 0) {
-//       counts[char]++;
-//     } else {
-//       counts[char] = 1;
-//     }
-//   }
-//   return counts;
-// }
-
-// function countCharacters(str) {
-//   const counts = {};
-//   for (let i = 0; i < str.length; i++) {
-//     // const char = str.charAt(i);
-//     const char = str[i].toLowerCase();
-//        if (char === " ") {
-//       continue; // skip white space character
-//     }
-//     else if(char in counts) {
-//       counts[char]++;
-//     } else {
-//       counts[char] = 1;
-//     }
-//   }
-//   return counts;
-// }
-
-
-// function countCharacters(str) {
-//   const counts = {};
-//   for (let char of str) {
-//     // const char = str.charAt(i);
-//     const char = char.toLowerCase();
-//        if (char === " ") {
-//       continue; // skip white space character
-//     }
-//     else if(counts[char] > 0) {
-//       counts[char]++;
-//     } else {
-//       counts[char] = 1;
-//     }
-//   }
-//   return counts;
-// }
-
-//  refactor 
-function countCharacters(str) {
-  const counts = {};
-  str.toLowerCase().split('').forEach(char => {
-    if (/[a-z0-9]/.test(char)) {
-      counts[char] = ++counts[char] || 1//(counts[char] || 0) + 1;
-    }
-  });
-  return counts;
+let instructor = {
+  firstName: "Muhammad",
+  isInstructor: true,
+  favoriteNumbers: [1,2,3,4]
 }
 
-countCharacters('aaaa')
-countCharacters('muhammad')
-countCharacters('My phone number is 123456')
+Object.keys(instructor);
+Object.entries(instructor);
+instructor.hasOwnProperty("firstName");
 
-
-
-
+/* 
+Big 0 of Object Methods
+Object.keys - O(N)
+Object.values - O(N)
+Object.entries - O(N)
+hasOwnProperty - O(1)
+*/
